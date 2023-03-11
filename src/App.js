27d8888cecddb2d1d39c69ecc561385638033641
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-03-11 16:22:17 +0200

    feat: add state to pizzablock

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
 
+import pizzas from "./assets/pizzas.json";
+
 import "./scss/app.scss";
 
 function App() {
@@ -17,12 +19,14 @@ function App() {
             <Categories />
             <Sort />
           </div>
-          <h2 className="content__title">Все пиццы</h2>
+          <h2 className="content__title">All Pizzas</h2>
           <div className="content__items">
-            <PizzaBlock title="Мексиканская" price={500}/>
-            <PizzaBlock title="hello" price={350}/>
-            <PizzaBlock />
-            <PizzaBlock />
+            {pizzas.map((obj) => (
+              <PizzaBlock
+                key={obj.id}
+                {...obj}
+              />
+            ))}
           </div>
         </div>
       </div>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function Categories() {
   const [activeIndex, setActiveIndex] = React.useState(2);
-  const onClickCategory = (index) => {
-    setActiveIndex(index);
-  };
+
+  const categories = ["All", "Meat", "Vegetarian", "Grill", "Acute", "Closed"];
+
   return (
     <div className="categories">
       <ul>
-        <li onClick={() => onClickCategory(0)} className={activeIndex === 0 ? "active" : ""}>Все</li>
-        <li onClick={() => onClickCategory(1)} className={activeIndex === 1 ? "active" : ""}>Мясные</li>
-        <li onClick={() => onClickCategory(2)} className={activeIndex === 2 ? "active" : ""}>Вегетарианская</li>
-        <li onClick={() => onClickCategory(3)} className={activeIndex === 3 ? "active" : ""}>Гриль</li>
-        <li onClick={() => onClickCategory(4)} className={activeIndex === 4 ? "active" : ""}>Острые</li>
-        <li onClick={() => onClickCategory(5)} className={activeIndex === 5 ? "active" : ""}>Закрытые</li>
+        {categories.map((value, i) => (
+          <li
+            key={i}
+            onClick={() => setActiveIndex(i)}
+            className={activeIndex === i ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
