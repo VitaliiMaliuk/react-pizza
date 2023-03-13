ed3413af65836effa85333381dac51abdf9ce356
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2023-03-13 14:46:26 +0200

    feat: add adaptive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,13 +10,13 @@ function Header() {
           <img width="38" src={logoSvg} alt="Pizza logo" />
           <div>
             <h1>React Pizza</h1>
-            <p>самая вкусная пицца во вселенной</p>
+            <p>the most delicious pizza in the universe</p>
           </div>
         </div>
         </Link>
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
-            <span>520 ₽</span>
+            <span>520 ₴</span>
             <div className="button__delimiter"></div>
             <svg
               width="18"
